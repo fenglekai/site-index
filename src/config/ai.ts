2ee15508e9d14263fbd251782b6830aeb43d702e
fenglekai <674640130@qq.com>
2024-10-31 16:42:35 +0800

    feat: 🎸 新增AI部分站点

--- src/config/ai.ts
+++ src/config/ai.ts
@@ -4,9 +4,39 @@ export default {
   navTitle: "AI/机器学习",
   children: [
     {
-      site: "GeekAI",
-      url: "https://docs.geekai.me/",
-      introduction: "一键搭建属于自己的 AI 助手平台",
+      site: "Kimi",
+      url: "https://kimi.moonshot.cn/",
+      introduction: "Kimi 是一个有着超大“内存”的智能助手，可以一口气读完二十万字的小说，还会上网冲浪，快来跟他聊聊吧",
+    },
+    {
+      site: "秒画AI",
+      url: "https://miaohua.sensetime.com/",
+      introduction: "会打字就会用的AI绘画神器，完美支持中文提示词，支持摄影、可爱、精致、赛博朋克、电影等超多风格，人人都可以是插画师！快速创作二次元、写实向等多种风格小姐姐！",
+    },
+    {
+      site: "哩布哩布AI",
+      url: "https://www.liblib.art/",
+      introduction: "AI绘画原创模型分享社区，10万+模型免费下载;原汁原味的webUI、comfyUI，在线AI绘图工具免费使用;还可在线进行模型训练。欢迎每一位创作者加入，共同探索AI绘画",
+    },
+    {
+      site: "可灵AI",
+      url: "https://klingai.kuaishou.com/",
+      introduction: "KLING AI，基于最先进的生成式 AI 方法，用于创建富有想象力的图像和视频的工具。",
+    },
+    {
+      site: "稿定",
+      url: "https://www.gaoding.com/",
+      introduction: "稿定一站式在线设计工具平台，提供全面的图片处理、设计工具和创建设计功能。支持AI抠图、AI消除、图片变清晰等智能处理，涵盖创意画布、H5页面制作、视频剪辑等多种工具，助您快速创建专业设计作品。丰富模板资源，让设计更高效便捷。",
+    },
+    {
+      site: "即梦AI",
+      url: "https://jimeng.jianying.com/",
+      introduction: "即梦AI是一个AI创作平台，可激发艺术创意、提升绘画和视频创作体验。您可以利用AI智能，将想象变为现实。即梦AI支持文字绘图、文字生成视频和图片生成视频，并提供创作灵感。让即梦AI开启您的AI生成艺术之旅，探索创造的无限可能！",
+    },
+    {
+      site: "堆友",
+      url: "https://d.design/",
+      introduction: "堆友是Alibaba Design打造的设计师全成长周期服务平台，围绕品质、效率、技能、成就、收入五大用户价值布局平台能力，全力服务设计师，旨在成为设计师的好朋友。",
     },
   ],
 } as NavLinkItem;
